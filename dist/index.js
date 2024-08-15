cafa9ee1dcc9e643f82bdd555d7e759c6b8cd2a3
Thashreef <thashreefkhan4@gmail.com>
2024-08-15 18:26:11 +0530

    first commit from the typescript

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// {Implicit method it is it automatically add the type of the data }
+// let username = "abijith "
+// username = "anil"
+// console.log("the username be like this ",username);
+let username = {
+    name: "aju",
+    age: 90,
+    salary: 9000
+};
+console.log("the username be this ", username);
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+// {Implicit method it is it automatically add the type of the data }
+// let username = "abijith "
+// username = "anil"
+// console.log("the username be like this ",username);
+
+import { log } from "console";
+
+
+// {Explicit method it is to  add the type of the data }
+
+// let username :string = "aju"
+// let abi :number = 89
+// let is_subscribed :boolean = true
+// let skills :string [] =["js","kill"]
+// let count :number []=[0,8,9]
+// let emptyarray :[] =[]
+// let object :{name:string,age:number}={
+//     name:"abin",
+//     age:90
+// }
+// console.log("the username is from explicit ",username,is_subscribed,skills,count,emptyarray);
+// console.log("the abi from the explicit is",abi,object);
+
+// {interface}
+// interface details {
+//     name:String;
+//     age:Number;
+//     salary:Number;
+//     getname :() => void;
+// }
+
+// let username :details ={
+//     name:"aju",
+//     age:90,
+//     salary:9000,
+//     getname() {
+//         console.log(this.name);
+        
+//     },
+// }
+// console.log("the username be this ",username);
+// username.getname()
+
+
