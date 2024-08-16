e73fe4974fb70c08547c3c814812cd45aa8fae39
Thashreef <thashreefkhan4@gmail.com>
2024-08-16 11:11:46 +0530

    typescript playlist upto first video

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,8 @@
 "use strict";
-// {Implicit method it is it automatically add the type of the data }
+// {Implicit method it is it automatically add the type of the data 
 // let username = "abijith "
 // username = "anil"
-// console.log("the username be like this ",username);
-let username = {
-    name: "aju",
-    age: 90,
-    salary: 9000
-};
-console.log("the username be this ", username);
+// console.log("the username be like this ",username);}
+let solution;
+solution = "failed";
+solution = "complete";
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-// {Implicit method it is it automatically add the type of the data }
+// {Implicit method it is it automatically add the type of the data 
 // let username = "abijith "
 // username = "anil"
-// console.log("the username be like this ",username);
-
-import { log } from "console";
+// console.log("the username be like this ",username);}
 
 
-// {Explicit method it is to  add the type of the data }
+
+// {Explicit method it is to  add the type of the data 
 
 // let username :string = "aju"
 // let abi :number = 89
@@ -19,9 +18,9 @@ import { log } from "console";
 //     age:90
 // }
 // console.log("the username is from explicit ",username,is_subscribed,skills,count,emptyarray);
-// console.log("the abi from the explicit is",abi,object);
+// console.log("the abi from the explicit is",abi,object);}
 
-// {interface}
+// {interface
 // interface details {
 //     name:String;
 //     age:Number;
@@ -39,6 +38,112 @@ import { log } from "console";
 //     },
 // }
 // console.log("the username be this ",username);
-// username.getname()
+// username.getname()}
+
+
+// {Type
+// type details ={
+//     name:String;
+//     age:Number;
+//     salary:Number;
+//     getname :() => void;
+// }
+
+// let username :details ={
+//     name:"aju",
+//     age:90,
+//     salary:9000,
+//     getname() {
+//         console.log(this.name);
+        
+//     },
+// }
+// console.log("the username be this ",username);
+// username.getname()}
+
+
+// {union operator
+// type details ={
+//     name:String;
+//     age:Number | string;
+//     salary:Number;
+//     getname :() => void;
+// }
+
+// let username :details ={
+//     name:"aju",
+//     age:"nine",
+//     salary:9000,
+//     getname() {
+//         console.log(this.name);
+        
+//     },
+// }
+// console.log("the username be this ",username);
+// username.getname()}
+
+// {optional operator
+// type details ={
+//     name:String;
+//     age:Number | string;
+//     salary:Number;
+//     getname?:() => void;
+// }
+
+// let username :details ={
+//     name:"aju",
+//     age:"nine",
+//     salary:9000,
+//     getname() {
+//         console.log(this.name);
+        
+//     },
+// }
+// console.log("the username be this ",username);
+// username.getname?.() }
+
+// {Functions
+
+// type Details = {
+//     name:String;
+//     age:number;
+//     salary:number;
+//     getname?:()=> void;
+// }
+// let userdetails:Details ={
+//     name:'aju',
+//     age:8,
+//     salary:9000
+// }
+
+// function getfunction({name,age}:{name:string,age:number}){
+//     return name
+// }
+
+// console.log("this be the output for the getfunction ",getfunction({name:"aju",age:9}));
+
+// function getfunction(userdetails:Details){
+//       return userdetails.age
+// } 
+// console.log("the getfunction be this ",getfunction(userdetails));
+
+
+// function getfunction(userdetails:Details):Details{
+//     return { name:userdetails.name,age:userdetails.age,salary:userdetails.salary};
+// }
+// console.log("the getfunction output be this ",getfunction(userdetails));}
+
+// {named types
+    // type status = "pending" | "complete" | "failed"
+
+    // let solution:status ;
+    // solution = "failed";
+    // solution = "complete";}
+
+
+   
+
+
+
 
 
